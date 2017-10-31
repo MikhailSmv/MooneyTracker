4fa35e91c215b001ec3b59d05d4cefe1fd1112fe
MikhailSmv <smirnov_mikhail_69@mail.ru>
2017-10-31 23:54:51 +0300

    Edit text, Button

--- app/src/main/java/com/loftscool/mooneytracker/MainActivity.java
+++ app/src/main/java/com/loftscool/mooneytracker/MainActivity.java
@@ -2,6 +2,11 @@ package com.loftscool.mooneytracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText titleEdit = findViewById(R.id.name);
+        final ImageButton addButton = findViewById(R.id.add);
+
+        titleEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            addButton.setEnabled(!TextUtils.isEmpty(charSequence));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
     }
+
 
     @Override
     protected void onStart() {
